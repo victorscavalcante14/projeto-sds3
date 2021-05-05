fddf8975884ff627ff331856af938b611f9cb222
Victor Cavalcante <victorscavalcante@gmail.com>
2021-05-05 18:48:37 -0300

    Pagination

---
+++ backend/src/main/java/com/victor/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.victor.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victor.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/victor/dsvendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.victor.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.victor.dsvendas.dto.SaleDTO;
+import com.victor.dsvendas.entities.Sale;
+import com.victor.dsvendas.repositories.SaleRepository;
+import com.victor.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		
+		return result.map(x -> new SaleDTO(x));
+	}
+}
